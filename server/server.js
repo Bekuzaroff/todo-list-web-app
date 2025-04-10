9305f7a6221df796693fc47eb68470dce30698ce
= <bekuzarovchermen@gmail.com>
2025-04-10 16:05:28 +0300

    Commit: mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+
+/*setting up config file with enviroment variables
+  DB_URL - FOR CONNECTION TO DB */
+dotenv.config({path: './main.env'});
+
 
 const PORT = 8000;
 app.listen(PORT, 'localhost', (err) => {
     if(err) throw err;
     console.log('server is started');
-})
+});
+
+/* connection to db by mongoose returns promise:
+    serverSelectionTimeoutMS - times out for connection*/ 
+const serverSelectionTimeoutMS = 5000;
+mongoose.connect(process.env.DB_URL, {
+    serverSelectionTimeoutMS
+}).then((value) => console.log('db is connected'))
+  .catch((reason) => console.log(reason))
+
 
